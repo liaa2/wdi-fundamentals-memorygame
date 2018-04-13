ca0091a13d73f06eb8b9649d56c05e20081084fb
Your Name <you@example.com>
2018-04-13 17:05:34 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,25 +23,35 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-	if (cardsInPlay.length ===2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
-	    alert ("You found a match!");
+	        alert ("You found a match!");
         } else {
-	    alert ("Sorry, try again.");
+	        alert ("Sorry, try again.");
         }	
-    }
 };
 
-var flipcard= function (cardId) {
+var flipcard= function () {
+	var cardId = this.getAttribute('data-id'); 
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-    checkForMatch ();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+    checkForMatch();
+    }
 };    
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipcard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
 
-flipcard(0);
-flipcard(2);
+createBoard();
 
 
